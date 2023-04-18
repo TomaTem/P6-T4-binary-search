c85bc34e9881fca7ee47aec253b08223798b8c8e
tomatem <tem7720@gmail.com>
2023-04-18 17:11:03 +0300

    done with extra tests

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
-
+function binarySearch(n, arr, start = 0, end = arr.length) {
+    let median = Math.floor((end + start) / 2);
+    if (n < arr[0] || n > arr[arr.length - 1]) {
+        return -1;
+    } else if (n == arr[median]) {
+        return median;
+    } else if (n < arr[median]) {
+        return binarySearch(n, arr, start, median - 1);
+    } else if (n > arr[median]) {
+        return binarySearch(n, arr, median + 1, end);
+    } else {
+        return -1;
+    }
 }
 
-module.exports = binarySearch
+module.exports = binarySearch;
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,34 @@
-const binarySearch = require('../index')
+const binarySearch = require('../index');
 
-test('Функция должна возвращать индекс элемента', () => {
-    
-})
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
+test('Функция должна возвращать индекс элемента в первой половине массива', () => {
+    expect(binarySearch(1, num)).toBe(0);
+    expect(binarySearch(2, num)).toBe(1);
+    expect(binarySearch('A', letters)).toBe(0);
+    expect(binarySearch('B', letters)).toBe(1);
+});
+test('Функция должна возвращать индекс элемента во второй половине массива', () => {
+    expect(binarySearch(9, num)).toBe(8);
+    expect(binarySearch(10, num)).toBe(9);
+    expect(binarySearch('G', letters)).toBe(6);
+    expect(binarySearch('H', letters)).toBe(7);
+});
+test('Функция должна возвращать индекс элемента в середине массива', () => {
+    expect(binarySearch(5, num)).toBe(4);
+    expect(binarySearch(6, num)).toBe(5);
+    expect(binarySearch('D', letters)).toBe(3);
+    expect(binarySearch('E', letters)).toBe(4);
+});
 test('Функция должна быть определена', () => {
-    
-})
-
+    expect(binarySearch).toBeDefined();
+});
 test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
-})
+    expect(binarySearch(22, num)).toBe(-1);
+    expect(binarySearch(-3, num)).toBe(-1);
+    expect(binarySearch('A', num)).toBe(-1);
+    expect(binarySearch('a', letters)).toBe(-1);
+    expect(binarySearch('U', letters)).toBe(-1);
+    expect(binarySearch(1, letters)).toBe(-1);
+});
